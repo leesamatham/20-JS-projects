341c4baa99fa05841a865302b6fe8802f0317268
Lee <132388212+leesamatham@users.noreply.github.com>
2023-05-10 12:25:27 -0400

    new files added

---
+++ JS_Project_1/Fetch_API_Cors_Errorsscript.js
@@ -0,0 +1,28 @@
+async function getQuote(){
+// Solution to the error 1---------------------------
+const proxyUrl='https://cors-anywhere.herokuapp.com/';
+// -------------------------------------------------
+const apiUrl='https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try{
+        const response=await fetch(proxyUrl+apiUrl);
+        const data=await response.json();
+        console.log(data);
+    }catch(error){
+        // Solution to error 2-----------------------
+        getQuote();
+        // ------------------------------------------
+        console.log('Ooops,No quote',error)
+    }
+}
+getQuote();
+
+
+
+// Step 1: Run the project
+// Got Error
+// Access to fetch at 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json' (redirected from 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json') from origin 'http://127.0.0.1:5500' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
+
+
+// Error 2
+
+// Ooops,No quote SyntaxError: Bad escaped character in JSON at position 53
--- JS_Project_1/fetch_API.js
+++ JS_Project_1/fetch_API.js
@@ -1,15 +1,28 @@
 // Fetch data from API
-
+// ===================================================
 // UI and functions attachment
 const quoteContainer=document.getElementById('quote-container');
 const quoteText=document.getElementById('quote');
 const authorText=document.getElementById('author');
 const twitterBtn=document.getElementById('twitter');
 const newQuoteBtn=document.getElementById('new-quote')
+const loader=document.getElementById('loader');
+let apiQuotes=[];
 
+// Show loading div
+function loading(){
+    loader.hidden=false;
+    quoteContainer.hidden=true;
+}
+// hide loading div
+function hiding(){
+    loader.hidden=true;
+    quoteContainer.hidden=false;
+}
 
 // Get Quotes from API
 async function getQuotes(){
+    loading();
     // Target data url-API URL
     const apiUrl='https://type.fit/api/quotes';
     try{
@@ -23,27 +36,24 @@ async function getQuotes(){
     }catch(error){
         // Catch Error here
     }
+    hiding();
 }
 
-getQuotes();
     // Backend code
-let apiQuotes=[];
 function newQuote(){
+    loading();
     // Display random quote using Math.random form apiQuotes array
     // Math.floor - gives the random less than given number
-const quote=apiQuotes[Math.floor(Math.random()* apiQuotes.length)];
-console.log(quote);
+    const quote=apiQuotes[Math.floor(Math.random()* apiQuotes.length)];
+    console.log(quote);
     // display on HTML page
     // check the quote length to determine styling
-
-
     if(quote.text.length>50){
         quoteText.classList.add('long-quote');
     }else{
         quoteText.classList.remove('long-quote');
-
     }
-quoteText.textContent=quote.text;
+    quoteText.textContent=quote.text;
     // forntend variable =backend object
     // check if author field is black and replace with unknown
     if(!quote.author){
@@ -51,6 +61,7 @@ quoteText.textContent=quote.text;
     }else{
         authorText.textContent=quote.author;
     }
+    hiding();
 }
 
 
@@ -61,6 +72,9 @@ function tweetQuote(){
     window.open(twitterUrl,'_blank');
 }
 
-
 newQuoteBtn.addEventListener('click',newQuote);
 twitterBtn.addEventListener('click',tweetQuote);
+
+getQuotes();
+// loading();
+// hiding();
